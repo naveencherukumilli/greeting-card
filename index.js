12161128b124595e4b470e6439fd25f5760683da
naveen <cherukumilli.naveen@gmail.com>
2023-03-18 13:55:35 +0000

    repo

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card">
+      <div>
+        <img
+          src="https://media-content.ccbp.in/ccbp_prod/media/profile_pic/b464604d-1d73-4146-8162-998b87778799.jpeg"
+          className="profile-image"
+        />
+        <p className="para1">Naveen Cherukumilli</p>
+        <p className="para2">G.B.R Degree college,Anaparthi.</p>
+        <img
+          src="https://ramlalautomobiles.com/wp-content/uploads/2018/11/11-1.png"
+          className="watch-image"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
